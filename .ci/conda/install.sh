f6e8294bf03a24890dc7344cd628f6db75c00c15
Xu Zhao <xzhao9@meta.com>
2025-09-24 10:21:57 -0400

    fix failure

--- .ci/conda/install.sh
+++ .ci/conda/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+set -ex
+
 wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /workspace/Miniconda3-latest-Linux-x86_64.sh
+cd /workspace
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 bash ./Miniconda3-latest-Linux-x86_64.sh -b -u -p /workspace/miniconda3
 
