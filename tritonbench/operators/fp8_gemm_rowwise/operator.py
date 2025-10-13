f867b108a9d73fc0de622cb6d6fa101cd3ca9beb
Randy Shuai <randysheriff@meta.com>
2025-10-13 11:43:50 -0700

    Disable triton_mm benchmark by default
    Summary: Disable testing on the op by default, due to incompatibility of some [shapes](https://github.com/meta-pytorch/tritonbench/issues/509).
    
    Differential Revision: D84527186

--- tritonbench/operators/fp8_gemm_rowwise/operator.py
+++ tritonbench/operators/fp8_gemm_rowwise/operator.py
@@ -201,7 +201,7 @@ class Operator(BenchmarkOperator):
             use_warp_specialization=self.warp_specialization,
         )
 
-    @register_benchmark(enabled=HAS_TRITON)
+    @register_benchmark(enabled=False)
     def _aoti_fp8_triton_mm(self, xq, wq, x_scale, w_scale, bias) -> Callable:
         return lambda: aoti_fp8_triton_mm(
             xq,
