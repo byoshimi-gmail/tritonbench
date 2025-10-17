e8a5f4c15b94ceb0c2465f8aa86c1c30814a3156
Xu Zhao <xzhao9@fb.com>
2025-10-17 10:02:25 -0700

    fix rope

--- tritonbench/operators/rope/operator.py
+++ tritonbench/operators/rope/operator.py
@@ -6,6 +6,7 @@ from transformers.models.llama.modeling_llama import (
     apply_rotary_pos_emb,
     LlamaRotaryEmbedding,
 )
+from transformers.models.llama.configuration_llama import LlamaConfig
 
 from tritonbench.utils.triton_op import (
     BenchmarkOperator,
@@ -44,7 +45,9 @@ class Operator(BenchmarkOperator):
 
     def prepare_input(self, hidden_size, seq_length):
         head_dim = hidden_size // self.num_q_heads
-        rotary_emb = LlamaRotaryEmbedding(head_dim, device=self.device)
+
+        llama_rotary_config = LlamaConfig(head_dim=head_dim)
+        rotary_emb = LlamaRotaryEmbedding(head_dim).to(self.device)
         q = (
             torch.randn(
                 (1, seq_length, self.num_q_heads, head_dim),
@@ -95,8 +98,9 @@ class Operator(BenchmarkOperator):
     def torch_compile_rotary_pos_emb_full_op(self, hidden_size, seq_length) -> Callable:
         q, k, cos, sin, pos_ids = self.prepare_input(hidden_size, seq_length)
         head_dim = hidden_size // self.num_q_heads
+        llama_rotary_config = LlamaConfig(head_dim=head_dim)
         compiled = torch.compile(
-            LlamaRotaryEmbedding(head_dim, device=self.device),
+            LlamaRotaryEmbedding(llama_rotary_config).to(self.device),
             mode="max-autotune-no-cudagraphs",
         )
         cos, sin = compiled(k, pos_ids)
